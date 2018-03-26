ea6f776a5db02e4e0e4926f9d488bf9e91d53e03
cybervoid <cybervoid41@gmail.com>
2018-03-26 07:18:25 -0400

    task changes

---
+++ DotNet/Learn_PLINQ_Async_Tasks/Learn_PLINQ_Async_Tasks/PLINQ/TasksStuff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn_PLINQ_Async_Tasks.PLINQ
+{
+    public class TasksStuff
+    {
+        public void Run()
+        {
+
+        }
+    }
+}
