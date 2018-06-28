8e3801c1a103cde28098dbd98009c36aa8a30894
cybervoid <4731255+cybervoid@users.noreply.github.com>
2018-06-28 07:31:12 -0700

    added regex console

---
+++ SoftwareEngineering/Fundamentals/Regex/Regex/RegexTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace Regex
+{
+    public class RegexTests
+    {
+        public RegexTests()
+        {
+        }
+
+        public bool Match(string pattern, string stringToCheck)
+        {
+            System.Text.RegularExpressions.Regex regex = new System.Text.RegularExpressions.Regex(pattern);
+            Match match = regex.Match(stringToCheck);
+            return match.Success;
+        }
+
+        public string Replace(string pattern, string stringToReplace, string replacementText)
+        {
+            System.Text.RegularExpressions.Regex regex = new System.Text.RegularExpressions.Regex(pattern);
+            return regex.Replace(stringToReplace, replacementText);
+        }
+    }
+}
