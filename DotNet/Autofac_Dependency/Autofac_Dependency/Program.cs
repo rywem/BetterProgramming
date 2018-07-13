e4bf9651f4f90ba8fe0663d796d997fe6242cbfb
cybervoid <4731255+cybervoid@users.noreply.github.com>
2018-07-13 11:41:22 -0700

    autofac project added.

---
+++ DotNet/Autofac_Dependency/Autofac_Dependency/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Autofac_Dependency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
