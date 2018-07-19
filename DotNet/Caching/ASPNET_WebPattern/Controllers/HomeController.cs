d6e615261f3d7915733a9b6d222333cc71b9be4f
Ryan <cybervoid41@gmail.com>
2018-07-19 08:57:29 -0700

    Created ASPNET_WebPattern caching project.

---
+++ DotNet/Caching/ASPNET_WebPattern/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNET_WebPattern.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (WebAppCache.UserContextData == null)
+                WebAppCache.Load();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
