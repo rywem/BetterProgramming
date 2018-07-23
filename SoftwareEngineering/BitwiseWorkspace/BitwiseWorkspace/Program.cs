24ea8fc9f8c27bd7e481e362b6d1505b2aeadeb2
cybervoid <4731255+cybervoid@users.noreply.github.com>
2018-07-23 14:23:35 -0700

    bitwise continued

---
+++ SoftwareEngineering/BitwiseWorkspace/BitwiseWorkspace/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BitwiseWorkspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitwiseBasicsReview bitwise = new BitwiseBasicsReview();
+            bitwise.Run();
+        }
+    }
+}
