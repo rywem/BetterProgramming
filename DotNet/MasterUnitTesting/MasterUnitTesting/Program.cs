b86ca7eee12947010e31a336066652bfa27ade44
cybervoid <4731255+cybervoid@users.noreply.github.com>
2018-07-25 11:31:25 -0700

    unit testing more examples started

--- DotNet/MasterUnitTesting/MasterUnitTesting/Program.cs
+++ DotNet/MasterUnitTesting/MasterUnitTesting/Program.cs
@@ -7,18 +7,18 @@ namespace MasterUnitTesting
         public static void Main(string[] args)
         {
             LearnFuncsActionsPreds.Runner runner = new LearnFuncsActionsPreds.Runner();
-            //runner.SimpleAnonymous();
-            //runner.FuncExample_WithParams();
-            //runner.ActionExample_Without_Params();
-            //runner.FuncExample_Without_Params();
-            runner.Func_Anonymous_Simple();
+            // runner.SimpleAnonymous();
+            // runner.FuncExample_WithParams();
+            // runner.ActionExample_Without_Params();
+            // runner.FuncExample_Without_Params();
+            //runner.Func_Anonymous_Simple();
             try
             {
                 #region "basic examples"
-                //UnitTestsLibrary.NunitExamples libclass = new UnitTestsLibrary.NunitExamples();
-                //libclass.FailExample();
-                //libclass.AssertExample();
-                //libclass.Warnings();
+                // UnitTestsLibrary.NunitExamples libclass = new UnitTestsLibrary.NunitExamples();
+                // libclass.FailExample();
+                // libclass.AssertExample();
+                // libclass.Warnings();
                 #endregion
 
                 UnitTestsLibrary.BankAccountTests accountTests = new UnitTestsLibrary.BankAccountTests();
---
+++ DotNet/MasterUnitTesting/UnitTestsLibrary/Solve.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace UnitTestsLibrary
+{
+    public class Solve
+    {
+        public Solve()
+        {
+        }
+    }
+}
