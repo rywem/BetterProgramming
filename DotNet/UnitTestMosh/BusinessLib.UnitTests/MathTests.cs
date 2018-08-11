4e01cd272a2048dbde3e5f91d4b7ef1c59173453
Ryan <cybervoid41@gmail.com>
2018-08-11 13:16:14 -0700

    added tests for the Math class

---
+++ DotNet/UnitTestMosh/BusinessLib.UnitTests/MathTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using BusinessLib.Fundamentals;
+
+namespace BusinessLib.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var math = new BusinessLib.Fundamentals.Math();
+            var result = math.Add(1, 2);
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+        {
+            var math = new BusinessLib.Fundamentals.Math();
+            var result = math.Max(2, 1);
+            Assert.That(result, Is.EqualTo(2));
+        }
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
+        {
+            var math = new BusinessLib.Fundamentals.Math();
+            var result = math.Max(2, 3);
+            Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
+        {
+            var math = new BusinessLib.Fundamentals.Math();
+            var result = math.Max(2, 2);
+            Assert.That(result, Is.EqualTo(2));
+        }
+    }
+}
---
+++ DotNet/UnitTestMosh/BusinessLib/Fundamentals/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLib.Fundamentals
+{
+    public class Math
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Max(int a, int b)
+        {
+            return (a > b) ? a : b;
+        }
+    }
+}
