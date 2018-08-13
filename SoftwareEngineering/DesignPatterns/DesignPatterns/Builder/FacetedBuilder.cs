5bd91754a103af9cc75d9899c0700e4221a1653d
cybervoid <4731255+cybervoid@users.noreply.github.com>
2018-08-13 10:37:04 -0700

    Faceted builder stub started

---
+++ SoftwareEngineering/DesignPatterns/DesignPatterns/Builder/FacetedBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace DesignPatterns.Builder.Faceted
+{
+
+    public class Person
+    {
+        //address
+        public string StreetAddress, PostCode, City;
+
+        //employment 
+        public string CompanyName, Position;
+        public int AnnualIncome;
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(PostCode)}:  {PostCode}, {nameof(City)}: {City}, {nameof(CompanyName)}: {CompanyName}, {nameof(Position)}: {Position}, {nameof(AnnualIncome)}: {AnnualIncome}";
+        }
+    }
+
+    public class PersonBuilder // facade
+    {
+        
+    }
+    public class FacetedBuilder
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}
