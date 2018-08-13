4bd03797af75f82fc9ef45081b047eb047457c31
cybervoid <4731255+cybervoid@users.noreply.github.com>
2018-08-13 10:37:54 -0700

    faceted partial completion

--- SoftwareEngineering/DesignPatterns/DesignPatterns/Builder/FacetedBuilder.cs
+++ SoftwareEngineering/DesignPatterns/DesignPatterns/Builder/FacetedBuilder.cs
@@ -19,7 +19,35 @@ namespace DesignPatterns.Builder.Faceted
 
     public class PersonBuilder // facade
     {
-        
+        //reference
+        protected Person person = new Person();
+
+    }
+
+    public class PersonJobBuilder : PersonBuilder
+    {
+        public PersonJobBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonJobBuilder At(string companyName)
+        {
+            person.CompanyName = companyName;
+            return this;
+        }
+
+        public PersonJobBuilder AsA(string position)
+        {
+            person.Position = position;
+            return this;
+        }
+
+        public PersonJobBuilder Earning(int amount)
+        {
+            person.AnnualIncome = amount;
+            return this;
+        }
     }
     public class FacetedBuilder
     {
