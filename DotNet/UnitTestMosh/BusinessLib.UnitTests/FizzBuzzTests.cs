4dccf84b837b831b648dd10b4f89404783a94997
Ryan <cybervoid41@gmail.com>
2018-08-13 08:40:14 -0700

    FizzBuzzTests implemented

--- DotNet/UnitTestMosh/BusinessLib.UnitTests/FizzBuzzTests.cs
+++ DotNet/UnitTestMosh/BusinessLib.UnitTests/FizzBuzzTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLib.Fundamentals;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,21 @@ using System.Threading.Tasks;
 
 namespace BusinessLib.UnitTests
 {
+    [TestFixture]
     public class FizzBuzzTests
     {
-
+        [Test]
+        [TestCase(3, "Fizz")]
+        [TestCase(9, "Fizz")]
+        [TestCase(12, "Fizz")]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(20, "Buzz")]
+        [TestCase(22, "22")]
+        public void GetOutput_WhenCalled_ReturnsAnswer(int input, string expectedResult)
+        {
+            var result = FizzBuzz.GetOutput(input);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
     }
 }
