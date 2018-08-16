66df4b7e00993c15d9066d9573729e24362fed99
cybervoid <4731255+cybervoid@users.noreply.github.com>
2018-08-16 07:26:40 -0700

    Factory implemented

--- SoftwareEngineering/DesignPatterns/DesignPatterns/Factories/Factory.cs
+++ SoftwareEngineering/DesignPatterns/DesignPatterns/Factories/Factory.cs
@@ -1,33 +1,17 @@
 ﻿using System;
 namespace DesignPatterns.Factories
 {
-
-
-    public static class PointFactory
-    {
-        /// <summary>
-        /// factory method design pattern
-        /// </summary>
-        /// <returns>The cartesian point.</returns>
-        /// <param name="x">The x coordinate.</param>
-        /// <param name="y">The y coordinate.</param>
-        public static Point NewCartesianPoint(double x, double y)
-        {
-            return new Point(x, y);
-        }
-
-        public static Point NewPolarPoint(double rho, double theta)
-        {
-            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-        }
-    }
-
     /// <summary>
     /// FactoryMethod
     /// </summary>
     public class Point
     {
+        // alternative approach:
+        //public static PointFactory Factory => new PointFactory();
         private double x, y;
+
+        public static Point Origin => new Point(0, 0); //creates a new instance every time accessed
+        public static Point Origin2 = new Point(0, 0); //better - creates a single instance that is re-used
         internal Point(double x, double y)
         {
             this.x = x;
@@ -37,12 +21,30 @@ namespace DesignPatterns.Factories
         {
             return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
         }
+        public static class Factory
+        {
+            /// <summary>
+            /// factory method design pattern
+            /// </summary>
+            /// <returns>The cartesian point.</returns>
+            /// <param name="x">The x coordinate.</param>
+            /// <param name="y">The y coordinate.</param>
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
+        }
     }
     public class FactoryDemo
     {
         public void Run()
         {
-            var point = PointFactory.NewPolarPoint(1.0, Math.PI / 2);
+            var point = Factory.NewPolarPoint(1.0, Math.PI / 2);
             Console.WriteLine(point);
         }
     }
