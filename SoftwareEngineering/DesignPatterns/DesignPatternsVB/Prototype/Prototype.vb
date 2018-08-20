535e72b167f6f0bb49c9cb80adbadd4b73d6ed3a
Ryan <cybervoid41@gmail.com>
2018-08-20 09:40:18 -0700

    prototype pattern with extension method DeepCopy serializers

--- SoftwareEngineering/DesignPatterns/DesignPatternsVB/Prototype/Prototype.vb
+++ SoftwareEngineering/DesignPatterns/DesignPatternsVB/Prototype/Prototype.vb
@@ -1,12 +1,35 @@
 ﻿Imports System.Console
+Imports System.IO
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.Serialization.Formatters.Binary
+Imports System.Xml.Serialization
 Imports DesignPatternsVB
 
-Public Interface IPrototype(Of T)
-    Function DeepCopy() As T
-End Interface
-Public Class Person
-    Implements IPrototype(Of Person)
 
+Module ExtensionMethods
+    <Extension()>
+    Public Function DeepCopySerializer(Of T)(self As T) As T
+        Dim stream = New MemoryStream()
+        Dim formatter = New BinaryFormatter()
+        formatter.Serialize(stream, self)
+        stream.Seek(0, SeekOrigin.Begin)
+        Dim copy As Object = formatter.Deserialize(stream)
+        stream.Close()
+        Return CType(copy, T)
+    End Function
+    <Extension()>
+    Public Function DeepCopyXML(Of T)(self As T) As T
+        Using ms As New MemoryStream
+            Dim ser = New XmlSerializer(GetType(T))
+            ser.Serialize(ms, self)
+            ms.Position = 0
+            Return CType(ser.Deserialize(ms), T)
+        End Using
+    End Function
+End Module
+
+<Serializable()>
+Public Class Person
     Public Property Names As String()
     Public Property Address As Address
 
@@ -27,14 +50,9 @@ Public Class Person
     Public Overrides Function ToString() As String
         Return $"{NameOf(Names)}: {String.Join(" ", Names)}, {NameOf(Address)} - {Address}"
     End Function
-
-    Public Function DeepCopy() As Person Implements IPrototype(Of Person).DeepCopy
-        Return New Person(Names, Address.DeepCopy())
-    End Function
 End Class
-
+<Serializable()>
 Public Class Address
-    Implements IPrototype(Of Address)
     Public Property StreetName As String
     Public Property HouseNumber As Integer
 
@@ -55,7 +73,7 @@ Public Class Address
         Me.HouseNumber = other.HouseNumber
     End Sub
 
-    Public Function DeepCopy() As Address Implements IPrototype(Of Address).DeepCopy
+    Public Function DeepCopy() As Address
         Return New Address(Me.StreetName, Me.HouseNumber)
     End Function
 End Class
@@ -67,8 +85,10 @@ Public Class Prototype
             sherlock.Names = New String() {"Sherlock", "Holmes"}
             sherlock.Address = New Address("Baker Street", 221)
         End With
-        Dim watson = New Person(sherlock)
-        watson.Names(0) = "Watson"
+        Dim watson = sherlock.DeepCopyXML()
+        watson.Names(0) = "John"
+        watson.Names(1) = "Watson"
+
         WriteLine(sherlock)
         WriteLine(watson)
     End Sub
