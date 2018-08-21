2a3af94a227ca07e258d284b1b6cdf2f54039d2e
Ryan <cybervoid41@gmail.com>
2018-08-21 11:13:43 -0700

    Prototype pattern moved, singleton started

--- SoftwareEngineering/DesignPatterns/DesignPatternsVB/Prototype/Prototype.vb
+++ SoftwareEngineering/DesignPatterns/DesignPatternsVB/Prototype/Prototype.vb
@@ -5,91 +5,92 @@ Imports System.Runtime.Serialization.Formatters.Binary
 Imports System.Xml.Serialization
 Imports DesignPatternsVB
 
+Namespace Prototype
+    Module ExtensionMethods
+        <Extension()>
+        Public Function DeepCopySerializer(Of T)(self As T) As T
+            Dim stream = New MemoryStream()
+            Dim formatter = New BinaryFormatter()
+            formatter.Serialize(stream, self)
+            stream.Seek(0, SeekOrigin.Begin)
+            Dim copy As Object = formatter.Deserialize(stream)
+            stream.Close()
+            Return CType(copy, T)
+        End Function
+        <Extension()>
+        Public Function DeepCopyXML(Of T)(self As T) As T
+            Using ms As New MemoryStream
+                Dim ser = New XmlSerializer(GetType(T))
+                ser.Serialize(ms, self)
+                ms.Position = 0
+                Return CType(ser.Deserialize(ms), T)
+            End Using
+        End Function
+    End Module
 
-Module ExtensionMethods
-    <Extension()>
-    Public Function DeepCopySerializer(Of T)(self As T) As T
-        Dim stream = New MemoryStream()
-        Dim formatter = New BinaryFormatter()
-        formatter.Serialize(stream, self)
-        stream.Seek(0, SeekOrigin.Begin)
-        Dim copy As Object = formatter.Deserialize(stream)
-        stream.Close()
-        Return CType(copy, T)
-    End Function
-    <Extension()>
-    Public Function DeepCopyXML(Of T)(self As T) As T
-        Using ms As New MemoryStream
-            Dim ser = New XmlSerializer(GetType(T))
-            ser.Serialize(ms, self)
-            ms.Position = 0
-            Return CType(ser.Deserialize(ms), T)
-        End Using
-    End Function
-End Module
+    <Serializable()>
+    Public Class Person
+        Public Property Names As String()
+        Public Property Address As Address
 
-<Serializable()>
-Public Class Person
-    Public Property Names As String()
-    Public Property Address As Address
+        Public Sub New()
 
-    Public Sub New()
+        End Sub
 
-    End Sub
+        Public Sub New(names As String(), address As Address)
+            Me.Names = names
+            Me.Address = address
+        End Sub
 
-    Public Sub New(names As String(), address As Address)
-        Me.Names = names
-        Me.Address = address
-    End Sub
+        Public Sub New(other As Person)
+            Me.Names = other.Names
+            Me.Address = New Address(other.Address)
+        End Sub
 
-    Public Sub New(other As Person)
-        Me.Names = other.Names
-        Me.Address = New Address(other.Address)
-    End Sub
+        Public Overrides Function ToString() As String
+            Return $"{NameOf(Names)}: {String.Join(" ", Names)}, {NameOf(Address)} - {Address}"
+        End Function
+    End Class
+    <Serializable()>
+    Public Class Address
+        Public Property StreetName As String
+        Public Property HouseNumber As Integer
 
-    Public Overrides Function ToString() As String
-        Return $"{NameOf(Names)}: {String.Join(" ", Names)}, {NameOf(Address)} - {Address}"
-    End Function
-End Class
-<Serializable()>
-Public Class Address
-    Public Property StreetName As String
-    Public Property HouseNumber As Integer
+        Public Sub New()
 
-    Public Sub New()
+        End Sub
 
-    End Sub
+        Public Sub New(streetName As String, houseNumber As Integer)
+            Me.StreetName = streetName
+            Me.HouseNumber = houseNumber
+        End Sub
+        Public Overrides Function ToString() As String
+            Return $"{NameOf(HouseNumber)} and {NameOf(StreetName)}: {HouseNumber} {StreetName}"
+        End Function
 
-    Public Sub New(streetName As String, houseNumber As Integer)
-        Me.StreetName = streetName
-        Me.HouseNumber = houseNumber
-    End Sub
-    Public Overrides Function ToString() As String
-        Return $"{NameOf(HouseNumber)} and {NameOf(StreetName)}: {HouseNumber} {StreetName}"
-    End Function
+        Public Sub New(other As Address)
+            Me.StreetName = other.StreetName
+            Me.HouseNumber = other.HouseNumber
+        End Sub
 
-    Public Sub New(other As Address)
-        Me.StreetName = other.StreetName
-        Me.HouseNumber = other.HouseNumber
-    End Sub
+        Public Function DeepCopy() As Address
+            Return New Address(Me.StreetName, Me.HouseNumber)
+        End Function
+    End Class
 
-    Public Function DeepCopy() As Address
-        Return New Address(Me.StreetName, Me.HouseNumber)
-    End Function
-End Class
+    Public Class Prototype
+        Public Sub Run()
+            Dim sherlock = New Person()
+            With sherlock
+                sherlock.Names = New String() {"Sherlock", "Holmes"}
+                sherlock.Address = New Address("Baker Street", 221)
+            End With
+            Dim watson = sherlock.DeepCopyXML()
+            watson.Names(0) = "John"
+            watson.Names(1) = "Watson"
 
-Public Class Prototype
-    Public Sub Run()
-        Dim sherlock = New Person()
-        With sherlock
-            sherlock.Names = New String() {"Sherlock", "Holmes"}
-            sherlock.Address = New Address("Baker Street", 221)
-        End With
-        Dim watson = sherlock.DeepCopyXML()
-        watson.Names(0) = "John"
-        watson.Names(1) = "Watson"
-
-        WriteLine(sherlock)
-        WriteLine(watson)
-    End Sub
-End Class
+            WriteLine(sherlock)
+            WriteLine(watson)
+        End Sub
+    End Class
+End Namespace
---
+++ SoftwareEngineering/DesignPatterns/DesignPatternsVB/Singleton/Singleton.vb
@@ -0,0 +1,39 @@
+﻿Option Explicit On
+Imports DesignPatternsVB
+Imports System.IO
+Imports MoreLinq
+
+Namespace Singleton
+    Public Interface IDatabase
+        Function GetPopulation(name As String) As Integer
+    End Interface
+    Public Class SingletonDatabase
+        Implements IDatabase
+
+        Dim capitals As Dictionary(Of String, Integer)
+        Public Sub New()
+            Console.WriteLine("Initializing Database")
+
+            ' LINQ... As C#:
+            ' capitals = File.ReadAllLines("capitals.txt")
+            '               .Batch(2)
+            '               .ToDictionary(list => list.ElementAt(0).Trim(), list => int.Parse(list.ElementAt(1)));
+
+            ' LINQ... As VB
+            Dim capitals = File.ReadAllLines("capitals.txt") _
+                            .Batch(2) _
+                            .ToDictionary(Function(list) list.ElementAt(0).Trim(), Function(list) Integer.Parse(list.ElementAt(1)))
+        End Sub
+
+        Public Function GetPopulation(name As String) As Integer Implements IDatabase.GetPopulation
+            Return capitals(name)
+        End Function
+    End Class
+
+    Public Class Singleton
+        Public Sub Run()
+            Dim db = New SingletonDatabase()
+            Dim info = db.GetPopulation("Tokyo")
+        End Sub
+    End Class
+End Namespace
