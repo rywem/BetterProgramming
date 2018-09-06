88e3df86df8818a27416a37b10178d82e89e14cd
cybervoid <4731255+cybervoid@users.noreply.github.com>
2018-09-06 09:35:44 -0700

    Added solution for ArtOfComputerProgramming

---
+++ SoftwareEngineering/ArtOfComputerProgramming/AOCPSolution/AOCPSolution/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AOCPSolution
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Section1_2.Algorithm_I algI = new Section1_2.Algorithm_I();
+            int output = algI.Run(4);
+            Console.WriteLine(output);
+        }
+    }
+}
