b80611c69ebf4bf630208c394a8838a1495e5de6
cybervoid <4731255+cybervoid@users.noreply.github.com>
2019-02-03 10:49:03 -0800

    Mix machine

---
+++ SoftwareEngineering/ArtOfComputerProgramming/AOCPSolution/AOCPSolution/Section1_2/Algorithm_E.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace AOCPSolution.Section1_2
+{
+    public class Algorithm_E
+    {
+        public int M { get; set; }
+        public int N { get; set; }
+        public Algorithm_E(int m,  int n)
+        {
+            if (m < 0)
+                throw ArgumentException("M cannot be less than 0");
+            if (n < 0)
+                throw ArgumentException("N cannot be less than 0");    
+            this.M = m;
+            this.N = n;
+        }
+    }
+}
