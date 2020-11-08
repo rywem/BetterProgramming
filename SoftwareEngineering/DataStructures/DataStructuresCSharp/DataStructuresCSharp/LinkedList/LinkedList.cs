e81186a1ae1e26997c1cfa3cf5b420c2899c13fe
Ryan <cybervoid41@gmail.com>
2020-11-08 14:55:59 -0800

    Basic LinkedList implementation

---
+++ SoftwareEngineering/DataStructures/DataStructuresCSharp/DataStructuresCSharp/LinkedList/LinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresCSharp.LinkedList
+{
+    public class LinkedList
+    {
+        public Node CreateNode(int value )
+        {
+            return new Node()
+            {
+                Value = value,
+                Next = null
+            };
+        }
+        public Node InsertAtBeginning(Node start, int value )
+        {
+            Node newHead = CreateNode(value);
+            newHead.Next = start;
+            return newHead;
+        }
+
+        public Node InsertAtEnd(Node start, int value)
+        {
+            Node temp = start;
+
+            while ( temp.Next != null )
+                temp = temp.Next;
+
+            temp.Next = CreateNode(value);
+            return start;
+        }
+
+        public void Print(Node node )
+        {
+            Node temp = node;
+
+            while ( true )
+            {
+                Console.WriteLine(temp.Value);
+                if ( temp.Next == null )
+                    break;
+                else
+                    temp = temp.Next;
+
+                
+            }
+            
+        }
+    }
+}
--- SoftwareEngineering/DataStructures/DataStructuresCSharp/DataStructuresCSharp/Program.cs
+++ SoftwareEngineering/DataStructures/DataStructuresCSharp/DataStructuresCSharp/Program.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using DataStructuresCSharp.LinkedList;
 namespace DataStructuresCSharp
 {
-   class Program
-   {
-      /// <summary>
-      /// A location to practice data structures.
-      /// </summary>
-      /// <param name="args"></param>
-      /// <remarks>https://www.csharpstar.com/</remarks>
-      static void Main( string[] args )
-      {
-         Console.WriteLine("Hello World!");
-      }
-   }
+    class Program
+    {
+        /// <summary>
+        /// A location to practice data structures.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <remarks>https://www.csharpstar.com/</remarks>
+        static void Main( string[] args )
+        {
+            DataStructuresCSharp.LinkedList.LinkedList list = new DataStructuresCSharp.LinkedList.LinkedList();
+            Node node = list.CreateNode(1);
+
+            node = list.InsertAtBeginning(node, 0);
+            list.InsertAtEnd(node, 2);
+            list.Print(node);
+
+        }
+    }
 }
